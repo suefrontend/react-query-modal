2d278226560418ab05f80bccdb8ffb36ee9457b6
Kayano Oyama <oyama_kayano@yahoo.co.jp>
2023-07-19 08:20:10 -0700

    Organize test - separate Rendering part and Behaviour part

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -1,9 +1,43 @@
-import { render, screen } from "@testing-library/react";
+import { getByDisplayValue, render, screen } from "@testing-library/react";
 import Home from "../pages";
 
 describe("Home page - rendering", () => {
-  it("should have Home Page text", () => {
-    render(<Home />);
-    screen.getByText("Home Page");
+  describe("Rendering", () => {
+    it("should have Home Page text", () => {
+      render(<Home />);
+      expect(screen.getByText("Home Page")).toBeInTheDocument();
+    });
+
+    it("should have button with Click Me", () => {
+      render(<Home />);
+      expect(
+        screen.getByRole("button", { name: "Click Me" })
+      ).toBeInTheDocument();
+    });
+
+    it("should have input field with label Enter Random Text", () => {
+      render(<Home />);
+      expect(screen.getByLabelText(/Enter Random Text/)).toBeInTheDocument();
+    });
+
+    it("should have input field with label Enter Specific Text", () => {
+      render(<Home />);
+      expect(screen.getByLabelText(/Specific/)).toBeInTheDocument();
+    });
+
+    it("should find input field by placeholder text Search", () => {
+      render(<Home />);
+      expect(screen.getByPlaceholderText(/Search/)).toBeInTheDocument();
+    });
+
+    it("should find input field by display value", () => {
+      render(<Home />);
+      screen.getByDisplayValue(/AUDI/);
+    });
+
+    it("should not find element with text: This is the text", () => {
+      render(<Home />);
+      expect(screen.queryByText("This is the text")).not.toBeInTheDocument();
+    });
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,30 @@
+import { useState } from "react";
 import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const [showText, setShowText] = useState(false);
   return (
     <div className={styles.main}>
       <h1>Home Page</h1>
+      <button>Click Me</button>
+      <div>
+        <label htmlFor="randomText">Enter Random Text: </label>
+        <input id="randomText" />
+      </div>
+      <div>
+        <label htmlFor="specificText">Enter Specific Text: </label>
+        <input id="specificText" />
+      </div>
+      <div>
+        <input placeholder="Search..." />
+      </div>
+      <div>
+        <input value="AUDI" />
+      </div>
+      <div>
+        <button onClick={() => setShowText(!showText)}>Show Text</button>
+        {showText && "This is the text"}
+      </div>
     </div>
   );
 }
